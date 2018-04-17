0246ba5f3168abf2998e368d8ed249b3339abf06
Kenneth Gajefski <kenneth.ag@outlook.com>
2018-04-17 17:24:39 -0400

    Added getter and setter functions to Customer class

--- src/MillennialMart/Customer.java
+++ src/MillennialMart/Customer.java
@@ -5,7 +5,7 @@ public class Customer {
     private static int totalCustomers = 0;
     private static double totalCost = 0;
     private static int totalItems = 0;
-    private int ID  ;
+    private int ID;
     private int items;
     private double cost;
     private Customer next;
@@ -26,5 +26,50 @@ public class Customer {
         this.items = items;
         this.cost = cost;
         next = null;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getItems() {
+        return items;
+    }
+
+    public void setItems(int items) {
+        this.items = items;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public Customer getNext() {
+        return next;
+    }
+
+    public void setNext(Customer next) {
+        this.next = next;
+    }
+
+    public boolean equals(Customer b)
+    {
+        if (this.ID == (b.ID) && this.next == (b.next))
+            return true;
+        else
+            return false;
+    }
+
+    public String toString()
+    {
+        return "ID: " + ID + "\nNumber of items: " + items + "\nCost: " + cost;
     }
 }
