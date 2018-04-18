8ea2cbe8d84d3a386236ad5e090dc66e4cd99dd7
Kenneth Gajefski <kenneth.ag@outlook.com>
2018-04-17 23:34:25 -0400

    Rest of functions in Main complete

--- src/MillennialMart/HW6.java
+++ src/MillennialMart/HW6.java
@@ -1,32 +1,50 @@
 package MillennialMart;
 
+import java.text.DecimalFormat;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class HW6 {
 
+    private static Customer top = null;
     private static int itemMin = 1;
     private static int itemMax = 40;
     private static int costMin = 1;
     private static int costMax = 200;
 
 
-    public static void insertCustomer(Customer ptr){
+    public static void insertCustomer(){
 
         int items = ThreadLocalRandom.current().nextInt(itemMin, itemMax + 1);
-        double cost = ThreadLocalRandom.current().nextDouble(costMin, costMax);
+        double tempCost = ThreadLocalRandom.current().nextDouble(costMin, costMax);
+        // Without going through the DecimalFormat for the cost, all values get truncated to one decimal spot of value 0
+        DecimalFormat df = new DecimalFormat("###.##");
+        double cost = Double.parseDouble(df.format(tempCost));
+        Customer temp = new Customer(items, cost);
 
-        Customer test = new Customer(items, cost);
+        temp.setNext(top);
+        top = temp;
+    }
+
+    public static Customer removeCustomer(){
+        Customer temp;
+
+        if (top == null)
+            return null;
+        else{
+            temp = top;
+            top = top.getNext();
+            return temp;
+        }
 
     }
 
-    /*public static Customer removeCustomer(){
-        Customer test;
-
-        return test;
-    }*/
-
     public static void printCustomers(){
+        Customer temp = top;
 
+        while (temp != null){
+            System.out.println(temp.toString());
+            temp = temp.getNext();
+        }
     }
 
     public static void main(String[] args) {
